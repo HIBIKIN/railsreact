10152939a434a3551d9655d89bd649cacab297e6
HIBIKIN <hi3bi1ki5@gmail.com>
2020-04-19 23:13:06 +0900

    create react component form...

---
+++ crud_front/src/Components/MainContainer.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import axios from 'axios';
+import ProductsContainer from './ProductsContainer'
+import FormContainer from './FormContainer'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+class MainContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: []
+    }
+  }
+
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/products')
+      .then((results) => {
+        console.log(results)
+        this.setState({ products: results.data })
+      })
+      .catch((data) => {
+        console.log(data)
+      })
+  }
+
+  render() {
+    return (
+      <div className='app-main'>
+        <FormContainer />
+        <ProductsContainer productData={this.state.products} />
+      </div>
+    )
+  }
+}
+
+export default MainContainer
