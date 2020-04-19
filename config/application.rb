744790b82f8a0dcf58fa6b17f90f610bef733c4b
HIBIKIN <hi3bi1ki5@gmail.com>
2020-04-19 21:23:10 +0900

    create react app

--- config/application.rb
+++ config/application.rb
@@ -33,3 +33,17 @@ module Railsreact
     config.api_only = true
   end
 end
+
+class Application < Rails::Application
+    config.load_defaults 5.1
+
+    config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*',
+        :headers => :any,
+        :methods => [:get, :post, :patch, :delete, :options]
+      end
+    end
+  end
